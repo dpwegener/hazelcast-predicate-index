fea70b3f6d395199d9f48297ccff0bdfe2a28acd
D. Wegener <dpwegener@gmail.com>
2018-01-06 13:12:41 -0600

    Remove unneeded code

--- src/main/java/com/wegener/hazelcast/StartClient.java
+++ src/main/java/com/wegener/hazelcast/StartClient.java
@@ -14,8 +14,6 @@ public class StartClient {
     public static void main( String[] args ) {
         ClientConfig clientConfig = new ClientConfig();
         HazelcastInstance client = HazelcastClient.newHazelcastClient( clientConfig );
-        IMap map = client.getMap( "customers" );
-        System.out.println( "Map Size:" + map.size() );
         IMap<String, Person> indexedMap = client.getMap("indexed");
         
         EntryObject eo = new PredicateBuilder().getEntryObject();
